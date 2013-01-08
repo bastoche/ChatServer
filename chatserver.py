cd6a925d03316afe6775df6024fef31783f9c8d7
Bastien Duret <bastien.duret@gmail.com>
2013-01-08 12:20:02 +0100

    basic echo server that accepts multiple clients

--- chatserver.py
+++ chatserver.py
@@ -1,14 +1,55 @@
 import socket
+import select
+
 
 # create a socket
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # bind a socket
-my_socket.bind(('', 5555))
+port = 5555
+server_socket.bind(('', port))
 
 # listen to connections
 max_queued_connections = 5
-my_socket.listen(max_queued_connections)
+server_socket.listen(max_queued_connections)
 
-# accept connections
-connection, address = my_socket.accept()
+
+client_sockets = []
+timeout = 0.05 # 50 milliseconds
+
+
+# main loop
+while True:
+    # check for new connections
+    queued_connections, wlist, xlist = select.select([server_socket], [], [], timeout)
+    
+    # accept new connections
+    for connection in queued_connections:
+        print("new connection !")
+        # accept the new connection
+        client_socket, address = connection.accept()
+        # add it to the client sockets list
+        client_sockets.append(client_socket)        
+        
+    # check for new commands if there are clients connected 
+    if client_sockets:        
+        # print("connected clients : {}".format(len(client_sockets)))
+        client_sockets_to_read, wlist, xlist = select.select(client_sockets, [], [], timeout)
+        
+        for client_socket in client_sockets_to_read:
+            print("new message to read")            
+            message = client_socket.recv(1024)
+            print("received message {} with length {}".format(message.decode(), len(message)))            
+            if message:
+                # the message is not empty, send back the received message
+                client_socket.send(message)                
+            else:                
+                # close the socket
+                client_socket.close()
+                client_sockets.remove(client_socket)
+                
+            
+        
+    
+server_socket.close()
+
