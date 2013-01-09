da86177673cfdc182cb58d402c66a0b183ccd988
Bastien Duret <bastien.duret@gmail.com>
2013-01-09 15:46:32 +0100

    unexpected deconnection handling + rebroadcast

--- chatserver.py
+++ chatserver.py
@@ -15,8 +15,8 @@ server_socket.listen(max_queued_connections)
 
 
 client_sockets = []
-timeout = 0.05 # 50 milliseconds
-
+timeout = 1 # 1 second
+buffer_length = 1024
 
 # main loop
 while True:
@@ -31,25 +31,30 @@ while True:
         # add it to the client sockets list
         client_sockets.append(client_socket)        
         
-    # check for new commands if there are clients connected 
+    # check for new messages if there are clients connected 
     if client_sockets:        
         # print("connected clients : {}".format(len(client_sockets)))
         client_sockets_to_read, wlist, xlist = select.select(client_sockets, [], [], timeout)
         
         for client_socket in client_sockets_to_read:
             print("new message to read")            
-            message = client_socket.recv(1024)
-            print("received message {} with length {}".format(message.decode(), len(message)))            
-            if message:
-                # the message is not empty, send back the received message
-                client_socket.send(message)                
-            else:                
-                # close the socket
-                client_socket.shutdown(socket.SHUT_WR)                
+            try:
+                message = client_socket.recv(buffer_length)
+                print("received message {} with length {}".format(message, len(message)))            
+                if message:
+                    #broadcasting message to all connected clients
+                    for client_socket in client_sockets:
+                        client_socket.send(message)
+                else:                                                                
+                    # close the socket
+                    print("shutdown and close")
+                    client_socket.shutdown(socket.SHUT_WR)                
+                    client_socket.close()
+                    client_sockets.remove(client_socket)
+            except ConnectionResetError:
+                print("connection reset for socket {}".format(client_socket))
                 client_socket.close()
-                client_sockets.remove(client_socket)
-                
-            
+                client_sockets.remove(client_socket)                            
         
     
 server_socket.close()
