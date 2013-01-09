a0a640fe78228671c01baaf7cdda2b8e45507e5c
Bastien Duret <bastien.duret@gmail.com>
2013-01-09 22:48:19 +0100

    improved the ChatServer class and added comments

--- chatserver.py
+++ chatserver.py
@@ -9,9 +9,10 @@ class ChatServer:
     def __init__(self):
         self.socket = None 
         self.connected_clients = []
-        self.logged_clients = []
     
-    def initialize(self, address, port):
+    def start(self, address, port):
+        """initialize the server socket using the specified address and port"""
+        
         # create a socket
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -22,7 +23,7 @@ class ChatServer:
         self.socket.listen(MAX_QUEUED_CONNECTIONS)
         
     def run(self):
-        # main loop
+        """main loop"""
         while True:
             self.check_new_connections()
             if self.connected_clients:
@@ -33,7 +34,7 @@ class ChatServer:
                 
             
     def check_new_connections(self):
-        # check for new connections
+        """check for new client connections"""
         queued_connections = select.select([self.socket], [], [], TIMEOUT)[0] 
     
         # accept new connections
@@ -41,10 +42,12 @@ class ChatServer:
             print("new connection")
             # accept the new connection
             client_socket = connection.accept()[0]
+                        
             # add it to the client sockets list
             self.connected_clients.append(client_socket)     
         
     def check_new_messages(self):
+        """check for new messages from the connected clients, handles disconnections too"""        
         client_sockets_to_read = select.select(self.connected_clients, [], [], TIMEOUT)[0]
         
         for client_socket in client_sockets_to_read:                
@@ -56,20 +59,26 @@ class ChatServer:
                     for client_socket in self.connected_clients:
                         client_socket.send(message)
                 else:                                                                
-                    # close the socket
-                    print("shutdown and close")
-                    client_socket.shutdown(socket.SHUT_WR)                
-                    client_socket.close()
-                    self.connected_clients.remove(client_socket)
+                    self.remove_client(client_socket)
             except ConnectionResetError:
                 print("connection reset for socket {}".format(client_socket))
-                client_socket.close()
-                self.connected_clients.remove(client_socket)                     
+                self.close_client(client_socket)
+                
+    def remove_client(self, client_socket):
+        """shutdown the socket then close it"""
+        client_socket.shutdown(socket.SHUT_WR)  
+        self.close_client(client_socket)
+                
+    def close_client(self, client_socket):
+        """close the socket and remove it from the list of connected clients"""
+        client_socket.close()
+        self.connected_clients.remove(client_socket)  
+     
 
-
+# main code
 chat_server= ChatServer()
 
-chat_server.initialize('', 5555)
+chat_server.start('', 5555)
 
 chat_server.run()
 
